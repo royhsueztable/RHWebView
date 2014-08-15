009d10be145fb76ec835996317dbb973fa42afc7
royhsueztable <royhsu@Roys-Retina-MacBook-Pro-13.local>
2014-08-15 09:54:53 +0800

    Add local and remote html.

--- RHWebView/RHViewController.m
+++ RHWebView/RHViewController.m
@@ -10,6 +10,8 @@
 
 @interface RHViewController ()
 
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
 @end
 
 @implementation RHViewController
@@ -18,12 +20,40 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self setup];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)setup
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//    [self loadContentRemotely];
+    
+    [self loadContentLocally];
+}
+
+- (void)loadContentRemotely
+{
+    NSURL *url = [NSURL URLWithString:@"http://appleday.cc"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    [self.webView loadRequest:request];
+}
+
+- (void)loadContentLocally
+{
+    NSString *localPageHTML = [[NSBundle mainBundle] pathForResource:@"LocalPage" ofType:@"html"];
+    NSString *htmlString = [NSString stringWithContentsOfFile:localPageHTML encoding:NSUTF8StringEncoding error:nil];
+    
+    htmlString = [self appendingJavaScript:htmlString];
+    
+    [self.webView loadHTMLString:htmlString baseURL:nil];
+}
+
+- (NSString *)appendingJavaScript:(NSString *)htmlString
+{
+    NSString *javaScript = @"<script>alert(\"This is an alert!!\");</script>";
+    
+    return [htmlString stringByAppendingString:javaScript];
 }
 
 @end
